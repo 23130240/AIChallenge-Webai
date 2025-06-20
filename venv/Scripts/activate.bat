da2085d3a8f0a3f1068d47418e6b5152d2289268
ThietNLU <23130314@st.hcmuaf.edu.vn>
2025-06-20 11:32:33 +0700

    Add libraries, complete .gitignore for clean repo

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\thirvo\OneDrive\NLU\Projects\object-detection-project\venv"
+set "VIRTUAL_ENV=C:\Users\thirvo\Projects\object-detection-project\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
